86a7c707df25d2d47d9ef27df5084b87e889ca05
Jiyeon <a1919361@gmail.com>
2021-11-25 15:33:28 +0900

    혼자 만들어본 페이지 컴포넌트 삭제

--- src/PageComponent.ts
+++
@@ -1,8 +0,0 @@
-export default class PageComponent{
-  page:HTMLElement;
-  constructor(){
-    this.page = document.createElement('div');
-    this.page.classList.add('page');
-    this.page.textContent = 'this is page component';
-  }
-}
--- src/main.ts
+++
@@ -1,3 +0,0 @@
-import AppComponent from './AppComponent';
-
-new AppComponent();
