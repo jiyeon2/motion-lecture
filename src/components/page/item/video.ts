5b9ca49c519319f559f3d317d720c4d709d55b25
Jiyeon <a1919361@gmail.com>
2021-11-25 17:50:47 +0900

    유투브 url 만들기 솔루션

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -1,24 +1,6 @@
 import { BaseComponent } from "../../component.js";
 
 
-function extractVideoId(url: string):string {
-  if (url.includes('youtu')){
-    return url.replace('https://youtu.be/','');
-  }
-  return url
-}
-
-function getYoutubeImbedSrc(url:string):string{
-  const videoId = extractVideoId(url);
-  return `https://www.youtube.com/embed/${videoId}`;
-}
-
-/**
- * 이용자가 유투브 영상 url을 가져오는 경우
- * 1. 주소창 https://www.youtube.com/watch?v=BcbmFxbdsJ0
- * 2. 동영상 url 복사 https://youtu.be/BcbmFxbdsJ0
- */
-
 export class VideoComponent extends BaseComponent<HTMLElement>{
   constructor(title: string, url: string){
     super(`<section class="video">
@@ -27,9 +9,31 @@ export class VideoComponent extends BaseComponent<HTMLElement>{
           </section>`);
 
     const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-    iframe.src = getYoutubeImbedSrc(url); 
+    iframe.src = this.convertToEmbeddedURL(url); 
 
     const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  /**
+   * 이용자가 유투브 영상 url을 가져오는 경우 embed url로 변환하여 리턴
+   * input
+   * 1. 주소창 https://www.youtube.com/watch?v=BcbmFxbdsJ0
+   * 2. 동영상 url 복사 https://youtu.be/BcbmFxbdsJ0
+   * 
+   * output https://www.youtube.com/embed/BcbmFxbdsJ0
+   * 
+   * 
+   * 정규표현식 https://regexr.com/5l6nr
+   */
+  private convertToEmbeddedURL(url: string):string {
+
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
