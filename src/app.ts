b0174878132defcd93f69bc42fce6b761de56a7a
Jiyeon <a1919361@gmail.com>
2021-11-25 16:16:03 +0900

    이미지 컴포넌트 솔루션까지

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 // 지금 프로젝트에서는 웹팩 안쓰고 있고, <script> 태그로 불러오지도 않고 html에서 type="module" 사용하고 있음
 // 이 경우 import 할 때 .js 확장자 붙여야 브라우저가 이해할 수 있다고 함
-import { PageComponent } from "./components/page.js";
-import { ImageComponent} from "./components/image.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 
-const img = new ImageComponent();
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement){
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
-    img.attatchTo(document.querySelector('.page')! as HTMLElement, 'beforeend');
+
+    const image = new ImageComponent('image title','https://picsum.photos/600/300');
+    image.attachTo(appRoot, 'beforeend');
 
   }
 }
