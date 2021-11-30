fcd3191ec4a462563a16c16b01caece3d5b74f0e
Jiyeon <a1919361@gmail.com>
2021-11-30 15:28:55 +0900

    페이지 아이템 컨테이너 만들기 혼자 안해보고 바로 솔루션따라함

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,9 +1,38 @@
-import { BaseComponent } from "../component.js";
+import { BaseComponent, Component } from "../component.js";
 
-export class PageComponent extends BaseComponent<HTMLUListElement>{
+// pageItem 과 page 둘다 동일한 메서드 가지고 있음 -> 인터페이스로 만들어 구현하도록 한다
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
+  constructor(){
+    super(`
+          <li class="page-item">
+              <section class="page-item__body"></section>
+              <div class="page-item__controls">
+                  <button class="close">&times;</button>
+              </div>
+          </li>
+          `);
+  }
+
+  addChild(child: Component){
+    const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable{
   constructor() {
     // 상속받는 자식 클래스에서는 super 통해 부모클래스의 생성자를 반드시 호출해야함
-    super('<ul class="page">This is Page Component@</ul>');
+    super('<ul class="page"></ul>');
+  }
+
+  addChild(section: Component){
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 
 }
