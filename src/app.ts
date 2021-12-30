37b063823b32743e4ebe9fb8bc435b705443563c
Jiyeon <a1919361@gmail.com>
2021-12-30 11:08:31 +0900

    디펜던시 인젝션 리팩토링 다시봄

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,24 @@ import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { Composable, PageComponent, PageItemComponent } from "./components/page/page.js";
+import { Composable, DarkPageItemComponent, PageComponent, PageItemComponent } from "./components/page/page.js";
+
+type Mode = 'dark' | 'light';
 
 class App {
+  private mode: Mode = 'light';
   private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement){
-    this.page = new PageComponent(PageItemComponent);
+    
+    // 함수 실행하는데 클래스를 인자로 넘기는게 낯설어서 이해를 잘 못했음(처음에는 인터페이스 넘기는줄)
+    // PageItemComponent는 인터페이스가 아니라 클래스임 
+    //    ㄴSectionContainerConstructor인터페이스(SectionContainer 인터페이스 구현하는 인스턴스를 리턴함)를 구현함
+    // PageItemComponent는 pagecomponent 인스턴스 내에서 addChild 할 때 pageItem 만들때 사용하는 클래스임
+    // PageComponent안에서 생성자 써도 상관은 없으나
+    // 범용성있는(외부에서 쓰고싶은 생성자 전달하고, 내부에서는 전달받은 생성자 사용하는) 형태로 만드려면 생성자를 전달하면 됨
+    // 이렇게 쓰면 모드가 바뀔때마다 페이지를 다시 생성해야겠지..??
+    this.page = new PageComponent(this.mode === 'light' ? PageItemComponent : DarkPageItemComponent);
+    // this.page = new PageComponent(DarkPageItemComponent);
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('image title','https://picsum.photos/600/300');
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -9,19 +9,26 @@ type OnCloseListener = () => void;
 
 
 // pageItemComponenet 의 필수 규격사항
-// .close 버튼 가지고 있다
-// setOnCloseListener 함수가 있다
+// 제한 : 컴포넌트여야 하고 addChild등 composable해야함 -> 해당 인터페이스 구현해야함
+// .close 버튼 가지고 있다 && setOnCloseListener api가 있어야함
 interface SectionContainer extends Component, Composable {
   setOnCloseListener(listener: OnCloseListener): void;
 }
 
 // 생성자를 정의하는 타입
 type SectionContainerConstructor = {
-  // 아무것도 받지 않는 생성자 -> SectionContainer 인스턴스 생성
+  // 아무것도 받지 않는 생성자(constructor) 함수 -> SectionContainer 인스턴스 반환
   new (): SectionContainer;
 }
 
+interface Closable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+// pageItemComponent 는 어떤 ui로 감쌀지 결정하는 컴포넌트(필수사항 - 닫기 버튼이 있어서 눌렀을때 닫혀야함 -> 규격사항interface로 지정)
 export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer{
+  // 아래와 같은가?
+// export class PageItemComponent extends BaseComponent<HTMLElement> implements Component, Composable, Closable{
   private closeListener?: OnCloseListener;
   constructor(){
     super(`
@@ -52,10 +59,51 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Sec
   }
 }
 
+
+export class DarkPageItemComponent extends BaseComponent<HTMLElement> implements Component, Composable, Closable{
+    private closeListener?: OnCloseListener;
+    constructor(){
+      super(`
+            <li class="page-item" style="background: black; color: white;">
+                <section class="page-item__body"></section>
+                <div class="page-item__controls">
+                    <button class="close">&times;</button>
+                </div>
+            </li>
+            `);
+  
+      const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+      closeBtn.onclick = () => {
+        this.closeListener && this.closeListener();
+      }
+    }
+  
+    addChild(child: Component){
+      const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+      child.attachTo(container);
+    }
+    
+    // 보통 이벤트등록 관련 함수는 생성자보다는 별도로 등록하는 함수를 만든다
+    // 필요에 의해 추후에 등록될 수 있으므로 인스턴스 생성하는 생성자함수보다 그 후에 유동성있게 추가할 수 있도록
+    // 생성자에는 인스턴스를 만들기 위해 반드시 필요한 요소만 인자로 받아오는 것이 좋다
+    setOnCloseListener(listener: OnCloseListener) {
+      this.closeListener = listener;
+    }
+
+  }
+
+
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable{
   // PageItemComponent 말고 다른 형태 DarkPageItemComponent 도 사용할 수 있고, AnimationPageItemComponent도 사용할수 있도록
   // dependency injection 하려면??
   
+  // SectionContainerConstructor은 SectionContainer 타입을 리턴하는데 여기서 
+  // type SectionContainerConstructor = { new (): SectionContainer; } 를 쓰면
+  // SectionContainer라는 오브젝트 만들 수 있는 클래스 타입 자체를 정의한것
+  
+  // pageItemConstructor: SectionConainer라고 쓰면
+  // 생성할 수 있는 클래스타입이 아닌 이미 생성된 오브젝트를 담을 수 있는 타입
+  // 이미 만들어진 오브젝트이므로 새로 생성 못하고, 그 오브젝트 내에 있는 함수나 변수에 접근 가능
   constructor(private pageItemContructor: SectionContainerConstructor) {
     // 상속받는 자식 클래스에서는 super 통해 부모클래스의 생성자를 반드시 호출해야함
     super('<ul class="page"></ul>');
