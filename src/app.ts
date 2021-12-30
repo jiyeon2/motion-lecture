b0aa34a91acea8e5565300106178a075d2ceda08
Jiyeon <a1919361@gmail.com>
2021-12-30 15:05:16 +0900

    다이얼로그 컨텐츠 채우기 시도

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 // 지금 프로젝트에서는 웹팩 안쓰고 있고, <script> 태그로 불러오지도 않고 html에서 type="module" 사용하고 있음
 // 이 경우 import 할 때 .js 확장자 붙여야 브라우저가 이해할 수 있다고 함
 import { Component } from "./components/component.js";
-import { InputDialog } from "./components/dialog/dialog.js";
+import { UrlInput, InputDialog, BodyInput } from "./components/dialog/dialog.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
+// import { ImageComponent } from "./components/page/item/image.js";
+// import { NoteComponent } from "./components/page/item/note.js";
+// import { TodoComponent } from "./components/page/item/todo.js";
+// import { VideoComponent } from "./components/page/item/video.js";
 import { Composable,  PageComponent, PageItemComponent } from "./components/page/page.js";
 
 
@@ -23,30 +27,107 @@ class App {
     // this.page = new PageComponent(DarkPageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent('image title','https://picsum.photos/600/300');
-    this.page.addChild(image);
+    // const image = new ImageComponent('image title','https://picsum.photos/600/300');
+    // this.page.addChild(image);
 
-    const video = new VideoComponent('video title!@!', 'https://youtu.be/BcbmFxbdsJ0');
-    this.page.addChild(video);
+    // const video = new VideoComponent('video title!@!', 'https://youtu.be/BcbmFxbdsJ0');
+    // this.page.addChild(video);
 
-    const note = new NoteComponent('note title@!~~~~', '노트 내용');
-    this.page.addChild(note);
+    // const note = new NoteComponent('note title@!~~~~', '노트 내용');
+    // this.page.addChild(note);
 
-    const todo = new TodoComponent('todo tile@#@', 'todo 내용');
-    this.page.addChild(todo);
+    // const todo = new TodoComponent('todo tile@#@', 'todo 내용');
+    // this.page.addChild(todo);
 
     const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
     imageBtn.addEventListener('click', () => {
       const dialog = new InputDialog();
 
+      const input = new UrlInput();
+      dialog.addChild(input);
+
       dialog.setOnCloseListener(() => {
         dialog.removeFrom(document.body);
       })
       dialog.setOnSubmitListener(() => {
-        // TODO: section 만들어서 페이지에 추가
+        const title = input.title.value;
+        const url = input.url.value;
+        const image = new ImageComponent(title,url);
+        this.page.addChild(image);
+        input.title.value = '';
+        input.url.value = '';
         dialog.removeFrom(document.body);
       })
+      
+      dialog.attachTo(document.body);
+    })
 
+    const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
+    videoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      const input = new UrlInput();
+      dialog.addChild(input);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      })
+      dialog.setOnSubmitListener(() => {
+        const title = input.title.value;
+        const url = input.url.value;
+        const image = new VideoComponent(title,url);
+        this.page.addChild(image);
+        input.title.value = '';
+        input.url.value = '';
+        dialog.removeFrom(document.body);
+      })
+      
+      dialog.attachTo(document.body);
+    })
+
+    const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
+    noteBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      const input = new BodyInput();
+      dialog.addChild(input);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      })
+      dialog.setOnSubmitListener(() => {
+        const title = input.title.value;
+        const body = input.body.value;
+        const image = new NoteComponent(title,body);
+        this.page.addChild(image);
+        input.title.value = '';
+        input.body.value = '';
+        dialog.removeFrom(document.body);
+      })
+      
+      dialog.attachTo(document.body);
+    })
+
+    const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
+    todoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      const input = new BodyInput();
+      dialog.addChild(input);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      })
+      dialog.setOnSubmitListener(() => {
+        const title = input.title.value;
+        const body = input.body.value;
+        const image = new TodoComponent(title,body);
+        this.page.addChild(image);
+        input.title.value = '';
+        input.body.value = '';
+        dialog.removeFrom(document.body);
+      })
+      
       dialog.attachTo(document.body);
     })
 
--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -4,6 +4,11 @@ import { Composable} from "../page/page.js";
 
 type onCloseListener = () => void;
 type onSubmitListener = () => void;
+
+// 동적으로 HTML요소 생성하는 컴포넌트이므로 BaseComponent(this.element 가짐)상속
+// 다이얼로그 안에 또다른 컴포넌트 담을 수 있으므로 Composable(addChild메서드) 인터페이스를 구현함
+// InputDialog.addChild 통해 외부에서 보여줄 컨텐츠 정할 수 있음
+// 버튼이 눌렸을때(닫기, 제출)의 동작도 외부에서 setOnCloseListener, setOnSubmitListener 로 받아온다
 export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
   closeListener?: onCloseListener;
   submitListener?: onSubmitListener;
@@ -41,5 +46,43 @@ export class InputDialog extends BaseComponent<HTMLElement> implements Composabl
   addChild(child: Component){
     const body = this.element.querySelector('#dialog__body')! as HTMLElement;
     child.attachTo(body);
+  }
+}
+
+export class UrlInput extends BaseComponent<HTMLElement> {
+  title: HTMLInputElement;
+  url: HTMLInputElement;
+  
+  constructor() {
+    super(`
+    <div class="url-input">
+      <label for="title">Title</label>
+      <input id="title" />
+      <label for="url">Url</label>
+      <input id="url" />
+    </div>
+  `)
+
+  this.title = this.element.querySelector('#title')! as HTMLInputElement;
+  this.url = this.element.querySelector('#url')! as HTMLInputElement;
+  }
+}
+
+export class BodyInput extends BaseComponent<HTMLElement> {
+  title: HTMLInputElement;
+  body: HTMLInputElement;
+  
+  constructor() {
+    super(`
+    <div class="url-input">
+      <label for="title">Title</label>
+      <input id="title" />
+      <label for="body">Body</label>
+      <input id="body" />
+    </div>
+  `)
+
+  this.title = this.element.querySelector('#title')! as HTMLInputElement;
+  this.body = this.element.querySelector('#body')! as HTMLInputElement;
   }
 }
